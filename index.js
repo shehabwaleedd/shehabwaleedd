2c19c594a89b9945ce23ebc7334aeff966c3ac5a
Shehab Waleed <104971270+shehabwaleedd@users.noreply.github.com>
2024-10-02 09:02:03 +0300

    Create index.js
    .

---
+++ index.js
@@ -0,0 +1,17 @@
+const simpleGit = require("simple-git"),
+  fs = require("fs");
+
+const ReadMe = `\`\`\`javascript
+    Hello - ${new Date()}
+\`\`\``;
+
+console.log("Init");
+
+fs.writeFile("README.md", ReadMe, "utf-8", (err) => {
+  if (err) throw err;
+});
+
+setInterval(() => {
+  console.log(`Committed ReadME - ${new Date()}`);
+  simpleGit().add(["*"]).commit(new Date()).push();
+}, 60 * 1000);
